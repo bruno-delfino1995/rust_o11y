9f4b06f0965425bef87c2ed3e893431617817073
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2022-12-03 00:52:49 -0300

    group fields by context/type

--- src/instrument/logs.rs
+++ src/instrument/logs.rs
@@ -1,12 +1,15 @@
 use std::collections::BTreeMap;
 use std::borrow::Borrow;
+use std::ops::Deref;
+use std::ops::DerefMut;
+use serde::Serialize;
 use tracing_subscriber::fmt::format::JsonFields;
 use tracing_subscriber::registry::LookupSpan;
 use tracing_subscriber::Layer;
 use tracing_subscriber::prelude::*;
 use std::fmt;
 use chrono::{SecondsFormat, Utc};
-use serde_json::{json, Value};
+use serde_json::{json, Map, Value};
 use tracing_core::{Subscriber, Event};
 use tracing_subscriber::fmt::{
 	self as formatter,
@@ -15,13 +18,59 @@ use tracing_subscriber::fmt::{
 	FormattedFields,
 };
 
-type Store = BTreeMap<String, Value>;
 
 pub trait Sub: Subscriber + for<'span> LookupSpan<'span> {}
 impl<T: Subscriber + for<'span> LookupSpan<'span>> Sub for T {}
 
 pub fn init<S: Sub>() -> impl Layer<S> {
 	CustomLayer
+}
+
+#[derive(Default, Clone)]
+struct Store(BTreeMap<String, Value>);
+
+impl Store {
+	fn new() -> Store {
+		Store(BTreeMap::new())
+	}
+
+	fn pull(&mut self, fields: Vec<&str>, from: &mut Store) {
+		for field in fields.into_iter() {
+			match from.remove_entry(field) {
+				Some((key, value)) => self.0.insert(key, value),
+				None => None,
+			};
+		}
+	}
+
+	fn push(&mut self, field: &str, from: Store) {
+		if from.is_empty() {
+			return;
+		}
+
+		self.0.insert(field.to_string(), json!(from));
+	}
+}
+
+impl Serialize for Store {
+	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+	where S: serde::Serializer {
+		self.0.serialize(serializer)
+	}
+}
+
+impl Deref for Store {
+	type Target = BTreeMap<String, Value>;
+
+	fn deref(&self) -> &Self::Target {
+		&self.0
+	}
+}
+
+impl DerefMut for Store {
+	fn deref_mut(&mut self) -> &mut Self::Target {
+		&mut self.0
+	}
 }
 
 pub struct CustomLayer;
@@ -59,23 +108,34 @@ impl<S: Sub> Layer<S> for CustomLayer {
 		values.record(&mut visitor);
 	}
 
+
 	fn on_event(&self, event: &tracing::Event<'_>, ctx: tracing_subscriber::layer::Context<'_, S>) {
-		let mut fields: Store = {
-			let mut context_fields = ctx.event_scope(event).map(context_fields).unwrap_or_default();
+		let fields: Store = {
+			let mut base = Store::new();
+			let mut runtime = Store::new();
+
+			let context_fields = ctx.event_scope(event).map(context_fields).unwrap_or_default();
+
 			let mut event_fields = event_fields(event);
+			base.pull(vec!["message"], &mut event_fields);
+
 			let mut metadata_fields = metadata_fields(event);
+			runtime.pull(vec!["file", "line", "target"], &mut metadata_fields);
+			base.pull(vec!["level"], &mut metadata_fields);
+
 			let mut additional_fields = additional_fields();
+			base.pull(vec!["timestamp"], &mut additional_fields);
+			runtime.pull(vec!["thread"], &mut additional_fields);
 
-			context_fields.append(&mut event_fields);
-			context_fields.append(&mut metadata_fields);
-			context_fields.append(&mut additional_fields);
+			base.push("context", context_fields);
+			base.push("data", event_fields);
+			base.push("runtime", runtime);
 
-			context_fields
+			base
 		};
 
-		let output = format(fields);
+		let output = json!(fields);
 
-		// println!("{}", serde_json::to_string_pretty(&output).unwrap());
 		println!("{}", output.to_string());
 	}
 }
@@ -107,7 +167,6 @@ fn metadata_fields(event: &tracing::Event<'_>) -> Store {
 
 	let data: Vec<(&str, Value)> = vec![
 		("target", json!(meta.target())),
-		("name", json!(meta.name())),
 		("level", json!(format!("{:?}", meta.level()))),
 		("line", json!(meta.line())),
 		("file", json!(meta.file())),
@@ -133,160 +192,6 @@ fn additional_fields() -> Store {
 	}
 
 	fields
-}
-
-fn format(mut fields: Store) -> Value {
-	let mut base = Store::new();
-	let mut runtime = Store::new();
-
-	pull(vec!["message", "timestamp", "level"], &mut fields, &mut base);
-	pull(vec!["thread", "file", "line", "target"], &mut fields, &mut runtime);
-
-	base.insert("runtime".to_string(), json!(runtime));
-	base.insert("context".to_string(), json!(fields));
-
-	json!(base)
-}
-
-fn pull(fields: Vec<&str>, from: &mut Store, into: &mut Store) {
-	for field in fields.into_iter() {
-		match from.remove_entry(field) {
-			Some((key, value)) => into.insert(key, value),
-			None => None,
-		};
-	}
-}
-
-fn set_path(
-	mut obj: &mut Value,
-	path: impl IntoIterator<Item=impl Borrow<str>>,
-	value: Value,
-) {
-	let mut path = path.into_iter();
-
-	// Start with nothing in "a" and the first item in "b".
-	let mut a;
-	let mut b = path.next();
-
-	loop {
-		// Shift "b" down into "a" and put the next item into "b".
-		a = b;
-		b = path.next();
-
-		// Move "a" but borrow "b" because we will use it on the next iteration.
-		match (a, &b) {
-			(Some(key), Some(_)) => {
-				// This level is an object, rebind deeper.
-				obj = &mut obj[key.borrow()];
-			}
-
-			(Some(s), None) => {
-				// This is the final string in the sequence.
-				*obj = Value::String(s.borrow().to_owned());
-				break;
-			}
-
-			// We were given an empty iterator.
-			(None, _) => { break; }
-		}
-	}
-}
-
-fn get_path(
-	obj: &Value,
-	path: impl IntoIterator<Item=impl Borrow<str>>,
-) -> Option<&Value> {
-	let mut path = path.into_iter().peekable();
-
-	if !obj.is_object() {
-		return None;
-	}
-
-	if path.peek().is_none() {
-		return None;
-	}
-
-	let mut value = Some(obj);
-	loop {
-		let key =
-			match path.next() {
-				Some(k) => k,
-				None => break value,
-			};
-
-		match value.and_then(|v| v.get(key.borrow().to_owned())) {
-			None => break None,
-			Some(v@Value::Object(_)) => value = Some(v),
-			v@Some(_) => {
-				if path.peek().is_some() {
-					break None
-				} else {
-					value = v;
-				}
-			},
-		}
-	}
-}
-
-#[cfg(test)]
-mod test {
-	use serde_json::{json, Value};
-
-	use super::{get_path, set_path};
-
-	mod get_path {
-		use super::*;
-
-		#[test]
-		fn none_when_not_object() {
-			let path = vec!["prop", "key"];
-
-			assert_eq!(get_path(&Value::Null, path.clone()), None);
-			assert_eq!(get_path(&json!(1), path.clone()), None);
-			assert_eq!(get_path(&json!(1.2), path.clone()), None);
-			assert_eq!(get_path(&json!(true), path.clone()), None);
-			assert_eq!(get_path(&json!("value"), path.clone()), None);
-		}
-
-		#[test]
-		fn none_when_path_not_found() {
-			assert_eq!(get_path(&json!({"a": {"b": {"c": 1}}}), vec!["c", "b", "a"]), None);
-			assert_eq!(get_path(&json!({"a": {"b": {"c": 1}}}), vec!["z", "y", "x"]), None);
-		}
-
-		#[test]
-		fn none_when_path_doesnt_end() {
-			assert_eq!(get_path(&json!({"a": {"b": {"c": 1}}}), vec!["a", "b", "c", "d"]), None);
-		}
-
-		#[test]
-		fn none_when_doesnt_find_object() {
-			assert_eq!(get_path(&json!({"a": 1}), vec!["a", "b"]), None);
-			assert_eq!(get_path(&json!({"a": {"b": 1}}), vec!["a", "b", "c"]), None);
-		}
-
-		#[test]
-		fn finds_in_depth() {
-			assert_eq!(get_path(&json!({"a": {"b": {"c": 1}}}), vec!["a", "b", "c"]), Some(&json!(1)));
-		}
-	}
-
-	mod set_path {
-		use super::*;
-
-		#[test]
-		fn changes_only_null_and_object() {
-			let path = vec!["prop", "key"];
-			vec!
-
-			assert_eq!(get_path(&Value::Null, path.clone()), None);
-			assert_eq!(get_path(&json!(1), path.clone()), None);
-			assert_eq!(get_path(&json!(1.2), path.clone()), None);
-			assert_eq!(get_path(&json!(true), path.clone()), None);
-			assert_eq!(get_path(&json!("value"), path.clone()), None);
-
-		}
-	}
 }
 
 struct CustomFieldStorage(Store);
